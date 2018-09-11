e5baf8c07bec1b127f0ac4529958a1d92843e559
naveenakallagunta <42571398+naveenakallagunta@users.noreply.github.com>
2018-09-11 10:22:45 +0530

    Update 19.py

--- 19.py
+++ 19.py
@@ -1,8 +1,8 @@
-a=int(raw_input())
+n=int(raw_input())
 fact=1
-if a==0:
+if n==0:
 	print "1"
-elif a>0:
-	for i in range(1,a+1):
-		fact=fact*i
+elif n>0:
+	for m in range(1,n+1):
+		fact=fact*m
 	print fact
